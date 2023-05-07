5a773bcdde7fffa46959975c7447d064774a09a0
Filipe Siqueira <filipedev040990@gmail.com>
2023-05-07 14:47:24 -0300

    feat: add create user route

--- src/infra/http/routes.ts
+++ src/infra/http/routes.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express'
+import { expressRouterAdapter } from '../adapters/express-router.adapter'
+import { buildCreateUserController } from '../factories/controllers/create-user.factory'
 
 const router = Router()
+
+router.post('/users', expressRouterAdapter(buildCreateUserController()))
 
 export { router }
