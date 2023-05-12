8dcfcc63ec107d3e381392a00cb08dd7701ece84
Filipe Siqueira <filipedev040990@gmail.com>
2023-05-12 08:04:32 -0300

    feat: ensure GetUserByIdUseCase reutn null if UserRepository.getById returns null

--- src/application/usecases/users/get-user-by-id.spec.ts
+++ src/application/usecases/users/get-user-by-id.spec.ts
@@ -1,10 +1,11 @@
+import { GetUserByIdUseCaseInterface } from '@/application/interfaces/get-user-by-id.interface'
 import { GetUserByIdRepositoryInterface } from '@/domain/interfaces/user-repository.interface'
 import { mock } from 'jest-mock-extended'
 
-export class GetUserByIdUseCase {
+export class GetUserByIdUseCase implements GetUserByIdUseCaseInterface {
   constructor (private readonly userRepository: GetUserByIdRepositoryInterface) {}
-  async execute (id: string): Promise<void> {
-    await this.userRepository.getById(id)
+  async execute (id: string): Promise<GetUserByIdUseCaseInterface.Output> {
+    return await this.userRepository.getById(id)
   }
 }
 
@@ -22,5 +23,13 @@ describe('GetUserByIdUseCase', () => {
 
     expect(userRepository.getById).toHaveBeenCalledTimes(1)
     expect(userRepository.getById).toHaveBeenCalledWith('any id')
+  })
+
+  test('should reutn null if UserRepository.getById returns null', async () => {
+    userRepository.getById.mockResolvedValueOnce(null)
+
+    const output = await sut.execute('any id')
+
+    expect(output).toBeNull()
   })
 })
