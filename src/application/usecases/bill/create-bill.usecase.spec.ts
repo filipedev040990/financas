a221e9a0d2ce9098a255c031bddfc2a5b84a805f
Filipe Siqueira <filipedev040990@gmail.com>
2023-05-15 11:59:50 -0300

    refactor: move production code to its own file

--- src/application/usecases/bill/create-bill.usecase.spec.ts
+++ src/application/usecases/bill/create-bill.usecase.spec.ts
@@ -3,31 +3,7 @@ import MockDate from 'mockdate'
 import { mock } from 'jest-mock-extended'
 import { BillRepositoryInterface } from '@/domain/interfaces/bill-repository.interface'
 import { UUIDGeneratorInterface } from '@/application/interfaces/uuid-generator.interface'
-
-export class CreateBillUseCase {
-  constructor (
-    private readonly uuidGenerator: UUIDGeneratorInterface,
-    private readonly billRepository: BillRepositoryInterface
-  ) {}
-
-  async execute (input: CreateBillUseCaseInterface.Input): Promise<CreateBillUseCaseInterface.Output> {
-    const output = await this.billRepository.create({
-      id: this.uuidGenerator.execute(),
-      type: input.type,
-      category: input.category,
-      expiration: input.expiration,
-      discount: input.discount,
-      interest: input.interest,
-      occurence: input.occurence,
-      payment_method: input.payment_method,
-      total_value: input.total_value,
-      observation: input?.observation,
-      createdAt: new Date()
-    })
-
-    return output
-  }
-}
+import { CreateBillUseCase } from './create-bill.usecase'
 
 const billRepository = mock<BillRepositoryInterface>()
 billRepository.create.mockResolvedValue({
---
+++ src/application/usecases/bill/create-bill.usecase.ts
@@ -0,0 +1,28 @@
+import { CreateBillUseCaseInterface } from '@/application/interfaces/create-bill-usecase.interface'
+import { UUIDGeneratorInterface } from '@/application/interfaces/uuid-generator.interface'
+import { BillRepositoryInterface } from '@/domain/interfaces/bill-repository.interface'
+
+export class CreateBillUseCase {
+  constructor (
+    private readonly uuidGenerator: UUIDGeneratorInterface,
+    private readonly billRepository: BillRepositoryInterface
+  ) {}
+
+  async execute (input: CreateBillUseCaseInterface.Input): Promise<CreateBillUseCaseInterface.Output> {
+    const output = await this.billRepository.create({
+      id: this.uuidGenerator.execute(),
+      type: input.type,
+      category: input.category,
+      expiration: input.expiration,
+      discount: input.discount,
+      interest: input.interest,
+      occurence: input.occurence,
+      payment_method: input.payment_method,
+      total_value: input.total_value,
+      observation: input?.observation,
+      createdAt: new Date()
+    })
+
+    return output
+  }
+}
