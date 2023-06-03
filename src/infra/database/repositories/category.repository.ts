5d7cade332c4f20befce2f3eb38bd4bc61d1355f
Filipe Siqueira <filipedev040990@gmail.com>
2023-06-03 11:03:08 -0300

    feat: implements create method of CategoryRepository

--- src/infra/database/repositories/category.repository.ts
+++ src/infra/database/repositories/category.repository.ts
@@ -1,8 +1,19 @@
 import { GetCategoryByIdRepositoryInterface } from '@/domain/interfaces/get-category-by-id-repository.interface'
 import { prismaClient } from '../prisma-client'
+import { CreateCategoryRepositoryInterface } from '@/domain/interfaces/create-category-repository.interface'
 
-export class CategoryRepository implements GetCategoryByIdRepositoryInterface {
+export class CategoryRepository implements GetCategoryByIdRepositoryInterface, CreateCategoryRepositoryInterface {
   async getById (id: string): Promise<GetCategoryByIdRepositoryInterface.Output> {
     return await prismaClient.category.findFirst({ where: { id } })
+  }
+
+  async create (input: CreateCategoryRepositoryInterface.Input): Promise<void> {
+    await prismaClient.category.create({
+      data: {
+        id: input.id,
+        name: input.name,
+        created_at: input.created_at
+      }
+    })
   }
 }
