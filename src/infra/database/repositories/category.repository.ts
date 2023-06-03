c0e183ecd869a4ea61e9dc694dc88ab9bbfa36a5
Filipe Siqueira <filipedev040990@gmail.com>
2023-06-03 12:36:50 -0300

    feat: implements update category method of Category Repository

--- src/infra/database/repositories/category.repository.ts
+++ src/infra/database/repositories/category.repository.ts
@@ -1,8 +1,9 @@
 import { GetCategoryByIdRepositoryInterface } from '@/domain/interfaces/get-category-by-id-repository.interface'
 import { prismaClient } from '../prisma-client'
 import { CreateCategoryRepositoryInterface } from '@/domain/interfaces/create-category-repository.interface'
+import { UpdateCategoryRepositoryInterface } from '@/domain/interfaces/update-category-repository.interface'
 
-export class CategoryRepository implements GetCategoryByIdRepositoryInterface, CreateCategoryRepositoryInterface {
+export class CategoryRepository implements GetCategoryByIdRepositoryInterface, CreateCategoryRepositoryInterface, UpdateCategoryRepositoryInterface {
   async getById (id: string): Promise<GetCategoryByIdRepositoryInterface.Output> {
     return await prismaClient.category.findFirst({ where: { id } })
   }
@@ -13,6 +14,18 @@ export class CategoryRepository implements GetCategoryByIdRepositoryInterface, C
         id: input.id,
         name: input.name,
         created_at: input.created_at
+      }
+    })
+  }
+
+  async update (input: UpdateCategoryRepositoryInterface.Input): Promise<void> {
+    await prismaClient.category.update({
+      data: {
+        name: input.name,
+        updated_at: input.updated_at
+      },
+      where: {
+        id: input.id
       }
     })
   }
