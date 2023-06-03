d18c34839b073b1a316e82dc06299e63b2e6506f
Filipe Siqueira <filipedev040990@gmail.com>
2023-06-03 10:50:34 -0300

    feat: ensure CreateCategoryController call CreateCategoryUseCase once and with correct values

--- src/adapters/controllers/categories/create-category.controller.spec.ts
+++ src/adapters/controllers/categories/create-category.controller.spec.ts
@@ -2,13 +2,17 @@ import { HttpRequest } from '@/adapters/types/http.type'
 import { CreateCategoryController } from './create-category.controller'
 import { badRequest } from '@/adapters/helpers/http.helper'
 import { MissingParamError } from '@/adapters/errors'
+import { mock } from 'jest-mock-extended'
+import { CreateCategoryUseCaseInterface } from '@/application/interfaces/create-category-usecase.interface'
+
+const createCategoryUseCase = mock<CreateCategoryUseCaseInterface>()
 
 describe('CreateCategoryController', () => {
   let sut: CreateCategoryController
   let input: HttpRequest
 
   beforeEach(() => {
-    sut = new CreateCategoryController()
+    sut = new CreateCategoryController(createCategoryUseCase)
     input = {
       body: {
         name: 'any name'
@@ -22,5 +26,12 @@ describe('CreateCategoryController', () => {
     const output = await sut.execute(input)
 
     expect(output).toEqual(badRequest(new MissingParamError('name')))
+  })
+
+  test('should call CreateCategoryUseCase once and with correct values', async () => {
+    await sut.execute(input)
+
+    expect(createCategoryUseCase.execute).toHaveBeenCalledTimes(1)
+    expect(createCategoryUseCase.execute).toHaveBeenCalledWith('any name')
   })
 })
--- src/adapters/controllers/categories/create-category.controller.ts
+++ src/adapters/controllers/categories/create-category.controller.ts
@@ -1,11 +1,14 @@
 import { MissingParamError } from '@/adapters/errors'
 import { badRequest } from '@/adapters/helpers/http.helper'
 import { HttpRequest } from '@/adapters/types/http.type'
+import { CreateCategoryUseCaseInterface } from '@/application/interfaces/create-category-usecase.interface'
 
 export class CreateCategoryController {
+  constructor (private readonly createCategoryUseCase: CreateCategoryUseCaseInterface) {}
   async execute (input: HttpRequest): Promise<any> {
     if (!input.body.name) {
       return badRequest(new MissingParamError('name'))
     }
+    await this.createCategoryUseCase.execute(input.body.name)
   }
 }
