8ac22516644575cba83ae885db7ca7d47fe165f5
Filipe Siqueira <filipedev040990@gmail.com>
2023-06-03 18:08:17 -0300

    feat: implements get category by id factories

---
+++ src/infra/factories/controllers/get-category-by-id.factory.ts
@@ -0,0 +1,10 @@
+import { ControllerInterface } from '@/application/interfaces/controller.interface'
+import { buildLogControllerDecorator } from './log-decorator.factory'
+import { GetCategoryByIdController } from '@/adapters/controllers/categories/get-category-by-id.controller'
+import { buildGetCategoryByIdUseCase } from '../usecases/get-category-by-id.factory'
+
+export const builgGetCategoryByIdController = (): ControllerInterface => {
+  const getCategoryByIdUseCase = buildGetCategoryByIdUseCase()
+  const controller = new GetCategoryByIdController(getCategoryByIdUseCase)
+  return buildLogControllerDecorator(controller)
+}
