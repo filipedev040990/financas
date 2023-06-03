63938f90ba7f2639896e6cff78445cdd5929a701
Filipe Siqueira <filipedev040990@gmail.com>
2023-06-03 14:39:53 -0300

    feat: ensure GetAllCategoriesController return all categories

--- src/adapters/controllers/categories/get-all-categories.controller.spec.ts
+++ src/adapters/controllers/categories/get-all-categories.controller.spec.ts
@@ -3,16 +3,37 @@ import { GetAllCategoriesUseCaseInterface } from '@/application/interfaces/get-a
 import { mock } from 'jest-mock-extended'
 
 const getAllCategoriesUseCase = mock<GetAllCategoriesUseCaseInterface>()
+const fakeCategories = [{
+  id: 'any id',
+  name: 'any name',
+  created_at: new Date('2023-01-01')
+}, {
+  id: 'another id',
+  name: 'another name',
+  created_at: new Date('2023-06-01'),
+  updated_at: new Date()
+}]
 
 describe('GetAllCategoriesController', () => {
   let sut: GetAllCategoriesController
 
   beforeAll(() => {
     sut = new GetAllCategoriesController(getAllCategoriesUseCase)
+    getAllCategoriesUseCase.execute.mockResolvedValue(fakeCategories)
   })
+
   test('should call GetAllCategoriesUseCase once', async () => {
     await sut.execute()
 
     expect(getAllCategoriesUseCase.execute).toHaveBeenCalledTimes(1)
+  })
+
+  test('should return all categories', async () => {
+    const output = await sut.execute()
+
+    expect(output).toEqual({
+      statusCode: 200,
+      body: fakeCategories
+    })
   })
 })
--- src/adapters/controllers/categories/get-all-categories.controller.ts
+++ src/adapters/controllers/categories/get-all-categories.controller.ts
@@ -1,8 +1,10 @@
+import { success } from '@/adapters/helpers/http.helper'
 import { GetAllCategoriesUseCaseInterface } from '@/application/interfaces/get-all-categories-usecase.interface'
 
 export class GetAllCategoriesController {
   constructor (private readonly getAllCategoriesUseCase: GetAllCategoriesUseCaseInterface) {}
   async execute (): Promise<any> {
-    await this.getAllCategoriesUseCase.execute()
+    const categories = await this.getAllCategoriesUseCase.execute()
+    return success(200, categories)
   }
 }
