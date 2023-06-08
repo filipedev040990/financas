a86c4ae38019f4cf57a554a297712904e320a4ed
Filipe Siqueira <filipedev040990@gmail.com>
2023-06-08 12:08:53 -0300

    feat: implements compare method on BcryptAdapter

--- src/infra/adapters/bcrypt.adapter.spec.ts
+++ src/infra/adapters/bcrypt.adapter.spec.ts
@@ -2,7 +2,8 @@ import { BcryptAdapter } from './bcrypt.adapter'
 import bcrypt from 'bcrypt'
 
 jest.mock('bcrypt', () => ({
-  hash: jest.fn().mockReturnValue('any hash')
+  hash: jest.fn().mockReturnValue('any hash'),
+  compare: jest.fn().mockReturnValue(true)
 }))
 
 describe('BcryptAdapter', () => {
@@ -13,16 +14,33 @@ describe('BcryptAdapter', () => {
     sut = new BcryptAdapter(salt)
   })
 
-  test('should call bcrypt.hash once and with correct values', async () => {
-    await sut.hash('any value')
+  describe('hash', () => {
+    test('should call bcrypt.hash once and with correct values', async () => {
+      await sut.hash('any value')
 
-    expect(bcrypt.hash).toHaveBeenCalledTimes(1)
-    expect(bcrypt.hash).toHaveBeenCalledWith('any value', 12)
+      expect(bcrypt.hash).toHaveBeenCalledTimes(1)
+      expect(bcrypt.hash).toHaveBeenCalledWith('any value', 12)
+    })
+
+    test('should return a hash', async () => {
+      const hash = await sut.hash('any value')
+
+      expect(hash).toBe('any hash')
+    })
   })
 
-  test('should return a hash', async () => {
-    const hash = await sut.hash('any value')
+  describe('compare', () => {
+    test('should call bcrypt.compare once and with correct values', async () => {
+      await sut.compare('any value', 'any hash')
 
-    expect(hash).toBe('any hash')
+      expect(bcrypt.compare).toHaveBeenCalledTimes(1)
+      expect(bcrypt.compare).toHaveBeenCalledWith('any value', 'any hash')
+    })
+
+    test('should return true', async () => {
+      const output = await sut.compare('any value', 'any hash')
+
+      expect(output).toBeTruthy()
+    })
   })
 })
--- src/infra/adapters/bcrypt.adapter.ts
+++ src/infra/adapters/bcrypt.adapter.ts
@@ -1,9 +1,14 @@
-import { HashGeneratorInterface } from '@/application/interfaces/crypto.interface'
+import { HashCompareInterface, HashGeneratorInterface } from '@/application/interfaces/crypto.interface'
 import bcrypt from 'bcrypt'
 
-export class BcryptAdapter implements HashGeneratorInterface {
+export class BcryptAdapter implements HashGeneratorInterface, HashCompareInterface {
   constructor (private readonly salt: number) {}
+
   async hash (value: string): Promise<string> {
     return await bcrypt.hash(value, this.salt)
+  }
+
+  async compare (value: string, hash: string): Promise<boolean> {
+    return await bcrypt.compare(value, hash)
   }
 }
